1400c1dc6e7669335de66d6d8b6ffee739769e92
Omar Manjarrez <omjrrz@outlook.com>
2016-10-15 10:39:44 -0500

    added information to cube, still not complete

--- Cubo.py
+++ Cubo.py
@@ -10,8 +10,42 @@ from Enums import *
 class Cubo:
     semantica = numpy.zeros((45, 45, 45))
     
+    # Variables: number, real, string, bool
+    # operadores: +, - , *, /, >=, <=, ==, <>, 
+    
     def __init__(self):
         self.semantica[toCode['number']][toCode['number']][toCode['+']] = toCode['number']
+        self.semantica[toCode['number']][toCode['real']]  [toCode['+']] = toCode['real']
+        self.semantica[toCode['number']][toCode['string']][toCode['+']] = toCode['error']
+        self.semantica[toCode['number']][toCode['bool']]  [toCode['+']] = toCode['error']
+        self.semantica[toCode['number']][toCode['number']][toCode['-']] = toCode['number']
+        self.semantica[toCode['number']][toCode['real']]  [toCode['-']] = toCode['real']
+        self.semantica[toCode['number']][toCode['string']][toCode['-']] = toCode['error']
+        self.semantica[toCode['number']][toCode['bool']]  [toCode['-']] = toCode['error']
+        self.semantica[toCode['number']][toCode['number']][toCode['*']] = toCode['number']
+        self.semantica[toCode['number']][toCode['real']]  [toCode['*']] = toCode['real']
+        self.semantica[toCode['number']][toCode['string']][toCode['*']] = toCode['error']
+        self.semantica[toCode['number']][toCode['bool']]  [toCode['*']] = toCode['error']
+        self.semantica[toCode['number']][toCode['number']][toCode['/']] = toCode['number']
+        self.semantica[toCode['number']][toCode['real']]  [toCode['/']] = toCode['real']
+        self.semantica[toCode['number']][toCode['string']][toCode['/']] = toCode['error']
+        self.semantica[toCode['number']][toCode['bool']]  [toCode['/']] = toCode['error']
+        self.semantica[toCode['number']][toCode['number']][toCode['>=']] = toCode['bool']
+        self.semantica[toCode['number']][toCode['real']]  [toCode['>=']] = toCode['bool']
+        self.semantica[toCode['number']][toCode['string']][toCode['>=']] = toCode['error']
+        self.semantica[toCode['number']][toCode['bool']]  [toCode['>=']] = toCode['error']
+        self.semantica[toCode['number']][toCode['number']][toCode['<=']] = toCode['bool']
+        self.semantica[toCode['number']][toCode['real']]  [toCode['<=']] = toCode['bool']
+        self.semantica[toCode['number']][toCode['string']][toCode['<=']] = toCode['error']
+        self.semantica[toCode['number']][toCode['bool']]  [toCode['<=']] = toCode['error']
+        self.semantica[toCode['number']][toCode['number']][toCode['==']] = toCode['bool']
+        self.semantica[toCode['number']][toCode['real']]  [toCode['==']] = toCode['bool']
+        self.semantica[toCode['number']][toCode['string']][toCode['==']] = toCode['error']
+        self.semantica[toCode['number']][toCode['bool']]  [toCode['==']] = toCode['error']
+        self.semantica[toCode['number']][toCode['number']][toCode['<>']] = toCode['bool']
+        self.semantica[toCode['number']][toCode['real']]  [toCode['<>']] = toCode['bool']
+        self.semantica[toCode['number']][toCode['string']][toCode['<>']] = toCode['error']
+        self.semantica[toCode['number']][toCode['bool']]  [toCode['<>']] = toCode['error']        
 
     def check(self, op1, op2, ope):
         code_op1 = toCode[op1]
@@ -21,6 +55,6 @@ class Cubo:
     
 x = Cubo()
 
-print(x.check('number', 'number', '+') )
+print(x.check('number', 'number', '*') )
         
 
--- Enums.py
+++ Enums.py
@@ -1,19 +1,27 @@
 toCode = {
+    'error': -1,
     'number': 1,
     'real': 2,
-    'bool': 11,
+    'bool': 3,
+    'string': 4,
     '+': 12,
     '-': 13,
     '*': 14,
     '/': 15,
-    '=': 16
+    '=': 16,
+    '>=': 17,
+    '<=': 18,
+    '==': 19,
+    '<>': 20
+    
 }
 
 toSymbol = {
-    0: 'error',
+    -1: 'error',
     1: 'number',
     2: 'real',
-    3: 'bool'
+    3: 'bool',
+    4: 'string'
 }
 
     
